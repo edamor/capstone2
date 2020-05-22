75178a879d8d371dc2fd818ee1833b1549c37532
amor.edlouise <amor.edlouise@gmail.com>
2020-05-22 11:42:23 +0800

    Updates

---
+++ precache-manifest.3f1611349afa3f80b09eeeb40b0cd559.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaded129815328402c331ebe3e397b6",
+    "url": "/capstone2/index.html"
+  },
+  {
+    "revision": "90a6273df0d1aa1fd588",
+    "url": "/capstone2/static/css/main.15916b10.chunk.css"
+  },
+  {
+    "revision": "8e3cf53b74068e6adc9f",
+    "url": "/capstone2/static/js/2.1826432d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/capstone2/static/js/2.1826432d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90a6273df0d1aa1fd588",
+    "url": "/capstone2/static/js/main.25eb9b15.chunk.js"
+  },
+  {
+    "revision": "b116912798f9d1617f44",
+    "url": "/capstone2/static/js/runtime-main.d4bd986c.js"
+  },
+  {
+    "revision": "dbd96bef2d29f5a19926be791e770930",
+    "url": "/capstone2/static/media/homePageBG-main.dbd96bef.png"
+  },
+  {
+    "revision": "05eb65404d9c785afed3640ddf48581d",
+    "url": "/capstone2/static/media/loginPageBG-main.05eb6540.png"
+  }
+]);
